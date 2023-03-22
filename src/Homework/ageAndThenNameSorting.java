c133fbf98fa67e755f7b04586c48d4897681326a
BinkaArushankina <albinarushankina@gmail.com>
2023-03-22 23:00:56 +0100

    Homework 22.03.23 Comparable Comparator Employee-salary,age,name,id,ageAndThenName.

--- src/Homework/ageAndThenNameSorting.java
+++ src/Homework/ageAndThenNameSorting.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class ageAndThenNameSorting implements Comparator<Employee> {
     public int compare(Employee one , Employee two){
         if(one.getAge()== two.getAge()){
-            one.getName().compareTo(two.getName());
+            return one.getName().compareTo(two.getName());
         }
         return one.getAge()- two.getAge();
     }
